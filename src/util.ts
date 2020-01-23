060f26402931a9cf53a756a1a35c18420c4da5dc
Adriaan <adriaanmarain300@gmail.com>
2020-01-23 10:54:36 +0100

    initial commit

---
+++ src/util.ts
@@ -0,0 +1,18 @@
+export function flareLog(message: string, isError: boolean = false) {
+    const formattedMessage = '@flareapp/flare-webpack-plugin-sourcemap: ' + message;
+
+    if (isError) {
+        console.error(formattedMessage);
+        return;
+    }
+
+    console.log(formattedMessage);
+}
+
+export function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (Math.random() * 16) | 0,
+            v = c == 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+}
